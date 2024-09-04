7c20d9df2bf5b72fe3a6b51e7b18321f3e042e09
NaskoksaN <136219696+NaskoksaN@users.noreply.github.com>
2024-09-04 07:33:00 +0300

    Delete 2.JavaScript Arrays and Strings/JavaScript-Front-End-SoftUni/1.JavaScript Syntax Fundamentals directory

--- 2.JavaScript Arrays and Strings/JavaScript-Front-End-SoftUni/1.JavaScript Syntax Fundamentals/Exercize/05-multiplicationTable.js
+++
@@ -1,7 +0,0 @@
-function solve(input){
-    for (let times = 1; times <= 10; times++) {
-        console.log(`${input} X ${times} = ${input * times}`);
-    }
-}
-
-solve(5);
--- 2.JavaScript Arrays and Strings/JavaScript-Front-End-SoftUni/1.JavaScript Syntax Fundamentals/Exercize/08-reversedChars.js
+++
@@ -1,8 +0,0 @@
-function solve (charA,charB,charC){
-    console.log(`${charC} ${charB} ${charA}`);
-}
-
-solve('A',
-'B',
-'C'
-);
